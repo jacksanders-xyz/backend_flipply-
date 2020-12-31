cd894b04ac4bb9ce2902e7d8c2301691a9f39c14
Jack Sanders <Guitarbreakerjs@gmail.com>
2020-12-31 00:52:38 -0700

    took away full_name

--- src/django_app/models.py
+++ src/django_app/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User, AbstractUser
 
 
 class User(AbstractUser):
-    full_name = models.CharField(max_length=50, null=True)
     stance = models.CharField(max_length=50, null=True)
 
     def __str__(self):
--- src/django_app/serializers.py
+++ src/django_app/serializers.py
@@ -6,20 +6,19 @@ from django.contrib.auth.hashers import make_password
 class UserObjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'password', 'full_name', 'stance')
+        fields = ('id', 'username', 'password', 'stance')
 
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'password', 'full_name', 'stance')
+        fields = ('id', 'username', 'password', 'stance')
 
     def create(self, validated_data):
         user = User.objects.create(
             username = validated_data['username'],
             password = make_password(validated_data['password']),
-            full_name = validated_data['full_name'],
             stance = validated_data['stance']
         )
 
