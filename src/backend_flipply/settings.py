e466e0463550805f0e45535e9d619153d3f9dc82
Jack Sanders <Guitarbreakerjs@gmail.com>
2021-03-17 22:17:00 -0600

    finally fetchin

--- src/backend_flipply/settings.py
+++ src/backend_flipply/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.26']
 
 
 # Application definition
@@ -140,13 +140,3 @@ SIMPLE_JWT = {
 }
 
 AUTH_USER_MODEL = "django_app.User"
-
-
-
-
-
-
-
-
-
-
